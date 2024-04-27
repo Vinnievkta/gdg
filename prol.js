ec46bfa155f335673ace8fb3436b2e6ef17cd843
Akansha kushwaha <rajkadv83@gmail.com>
2024-04-27 08:00:25 +0530

    init

---
+++ prol.js
@@ -0,0 +1,6 @@
+const editProfileBtn = document.querySelector('.edit-profile-btn');
+
+editProfileBtn.addEventListener('click', () => {
+  // Code to edit profile goes here
+  console.log('Edit profile button clicked');
+});
